bb89dcf99a2dee5b2b2ffe40f489948a6572f5b8
Abhilash <abhilash.vc888@gmail.com>
2025-06-20 01:09:45 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 const app = express();
-const port = 3001;
+const port = 10000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
